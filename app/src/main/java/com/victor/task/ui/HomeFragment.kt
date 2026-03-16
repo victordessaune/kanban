dfbaf5e462c775361263beb54316bbee511071af
victordessaune <victorramosdessaune@gmail.com>
2026-03-16 18:16:40 -0300

    Inicio do passo 3

--- app/src/main/java/com/victor/task/ui/HomeFragment.kt
+++ app/src/main/java/com/victor/task/ui/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.victor.task.R
 import com.victor.task.databinding.FragmentHomeBinding
+import com.victor.task.ui.adapter.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
 
@@ -20,6 +22,26 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs()
+    }
+
+    private fun initTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager){ tab, postion ->
+            tab.text = getString(pageAdapter.getTitle(postion))
+        }.attach()
     }
 
     override fun onDestroyView() {
