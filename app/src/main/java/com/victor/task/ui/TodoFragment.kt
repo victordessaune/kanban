33dcb649f5f42ff4b156b5b7a34a5409cae266e9
victordessaune <victorramosdessaune@gmail.com>
2026-03-20 16:18:29 -0300

    Fim do passo 3 e início do 4

--- app/src/main/java/com/victor/task/ui/TodoFragment.kt
+++ app/src/main/java/com/victor/task/ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.victor.task.R
 import com.victor.task.databinding.FragmentHomeBinding
 import com.victor.task.databinding.FragmentTodoBinding
@@ -21,6 +22,17 @@ class TodoFragment : Fragment() {
     ): View {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton2.setOnClickListener {
+            findNavController().navigate((R.id.action_homeFragment_to_formTaskFragment))
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/victor/task/util/Extensions.kt
@@ -0,0 +1,8 @@
+package com.victor.task.util
+
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+fun Fragment.initToolbar(toolbar: Toolbar){
+
+}
