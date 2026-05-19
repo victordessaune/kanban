a101da81b245c04447c5835abdc9c297f5fe632f
victordessaune <victorramosdessaune@gmail.com>
2026-05-19 16:23:01 -0300

    Tarefas agora são inseridas no banco de dados

--- app/src/main/java/com/victor/task/data/model/Task.kt
+++ app/src/main/java/com/victor/task/data/model/Task.kt
@@ -5,8 +5,8 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Task (
-    val id: String,
-    val description: String,
+    var id: String = "",
+    var description: String = "",
 
-    val status: Status = Status.TODO
+    var status: Status = Status.TODO
 ): Parcelable
--- app/src/main/java/com/victor/task/ui/FormTaskFragment.kt
+++ app/src/main/java/com/victor/task/ui/FormTaskFragment.kt
@@ -6,7 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 import com.victor.task.R
+import com.victor.task.data.model.Status
+import com.victor.task.data.model.Task
 import com.victor.task.databinding.FragmentDoneBinding
 import com.victor.task.databinding.FragmentFormTaskBinding
 import com.victor.task.databinding.FragmentRegisterBinding
@@ -17,6 +26,16 @@ class FormTaskFragment : Fragment() {
 
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var task: Task
+
+    private var NewTask: Boolean = true
+
+    private var status: Status = Status.TODO
+
+    private lateinit var reference: DatabaseReference
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +48,10 @@ class FormTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+
+        reference = Firebase.database.reference
+        auth = Firebase.auth
+
         initListener()
     }
 
@@ -36,15 +59,56 @@ class FormTaskFragment : Fragment() {
         binding.buttonSave.setOnClickListener {
             validateData()
         }
+
+        binding.radioGroup.setOnCheckedChangeListener { _, id -> status =
+            when(id) {
+                R.id.rbTodo -> Status.TODO
+                R.id.rbDoing -> Status.DOING
+                else -> Status.DONE
+            }
+        }
     }
 
     private fun validateData() {
         val description = binding.editTextDescricao.text.toString().trim()
         if (description.isNotBlank()) {
-            Toast.makeText(requireContext(), "Tudo OK!", Toast.LENGTH_SHORT).show()
+            binding.progressBar.isVisible = true
+
+            if(NewTask) task = Task()
+
+            task.id = reference.database.reference.push().key ?:""
+            task.description = description
+            task.status = status
+
+            saveTask()
         } else {
             showBottomSheet(message = getString(R.string.description_empty_form_task_fragment))
         }
+    }
+
+    private fun saveTask() {
+        reference
+            .child("task")
+            .child(auth.currentUser?.uid ?: "")
+            .child(task.id)
+            .setValue(task).addOnCompleteListener { result ->
+                if (result.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.text_save_sucess_form_task_fragment,
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    if (NewTask) {
+                        findNavController().popBackStack()
+                    } else {
+                        binding.progressBar.isVisible = false
+                    }
+                } else {
+                    binding.progressBar.isVisible = false
+                    showBottomSheet(message = getString(R.string.error))
+                }
+            }
     }
 
     companion object
